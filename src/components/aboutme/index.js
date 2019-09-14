bc16beac16b4a955b9b6ba2012e3a6e31deaa6ab
bloomy215 <bmbloomquist@gmail.com>
2019-09-14 00:23:36 -0500

    adjustment to responsiveness of about me page

--- src/components/aboutme/index.js
+++ src/components/aboutme/index.js
@@ -5,12 +5,12 @@ const AboutMe = ( ) => {
   return (
     <Container className = "mt-5">
       <Row className = "justify-content-center">
-        <Col xs = { 12 } sm = { 8 } className = "header">
+        <Col xs = { 12 } sm = { 9 } className = "header">
           <h2 className = "engraved pt-2 text-center">About Me</h2>
         </Col>
       </Row>
       <Row className = "justify-content-center">
-        <Col xs = { 12 } sm = { 8 } className = "header aboutMe p-3">
+        <Col xs = { 12 } sm = { 9 } className = "header aboutMe p-3">
           <img class = "float-right bioPic p-3 img-fluid" src = "assets/images/biopic.png" alt = "bryan bloomquist" />
           <p>
             I enlisted in the Army Reserves after high school so I could go to college because that is what you were supposed to do.  I changed majors three times because at 18 I had no idea what I wanted to do with my life, and before I could graduate I was activated and sent to Iraq.  I spent most of 2003 driving a fuel truck through Iraq and Kuwait.
@@ -27,7 +27,7 @@ const AboutMe = ( ) => {
         <Col xs = { 12 } sm = { 4 } className = "header contactInfo engraved p-3">
           <h5>Phone: <a href = "tel:1-612-226-8339">612-226-8339</a></h5>
         </Col>
-        <Col xs = { 12 } sm = { 4 } className = "header contactInfo engraved p-3">
+        <Col xs = { 12 } sm = { 5 } className = "header contactInfo engraved p-3">
           <h5>email: <a href = "mailto:bmbloomquist@gmail.com" title = "Email" target = "_top">bmbloomquist@gmail.com</a></h5>
         </Col>
       </Row>
