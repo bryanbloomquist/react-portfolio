e5f79d14ee721b570db542573cd064c1c02692f5
bloomy215 <bmbloomquist@gmail.com>
2019-09-17 15:49:43 -0500

    Updates

---
+++ precache-manifest.e4443025775bd944865d71129e7ad94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b66a33011518ec514080475869566f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4596e29fa1ac9e974bff",
+    "url": "/react-portfolio/static/css/main.a3cd381c.chunk.css"
+  },
+  {
+    "revision": "4427e45e0519c4d0a705",
+    "url": "/react-portfolio/static/js/2.fa4a08c3.chunk.js"
+  },
+  {
+    "revision": "4596e29fa1ac9e974bff",
+    "url": "/react-portfolio/static/js/main.f138fcff.chunk.js"
+  },
+  {
+    "revision": "1a54709fff9f8b997791",
+    "url": "/react-portfolio/static/js/runtime~main.0a4edb9a.js"
+  },
+  {
+    "revision": "f7aec3a086f86effb82cfd1ad9f1e80a",
+    "url": "/react-portfolio/static/media/background.f7aec3a0.jpg"
+  }
+]);
